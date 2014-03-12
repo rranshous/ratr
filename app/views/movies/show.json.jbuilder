bfd8a806dda694625a97537e5d49775744d6ace0
Robby Ranshous <rranshous@gmail.com>
2014-03-12 12:54:49 -0400

    scaffolded movie model

---
+++ app/views/movies/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @movie, :id, :title, :upvotes, :description, :created_at, :updated_at
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ratr::Application.config.secret_key_base = '7dcbab1f2efecc6b9f8accbc1ede0c773995318ab4fc7228e2dd5a223f983db9a155c23f87d8500b8e0855f310a4c522c12f4a97c2c72f92ff11851705ab0a63'
